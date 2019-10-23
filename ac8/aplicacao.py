d802d66d0c066146557c76585487255067d5e4c4
adrialmaia <adriano.amaia@gmail.com>
2019-10-22 22:02:47 -0300

    Arquivo 03

--- ac8/aplicacao.py
+++ ac8/aplicacao.py
@@ -4,3 +4,32 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 app.run()
+
+from flask import Flask, render_template 
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('detalhesEletronica.html')
+app.run()
+
+from flask import Flask, render_template 
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('detalhesInformáticaIndustrial.html')
+app.run()
+
+from flask import Flask, render_template 
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('detalhesMecatronica.html')
+app.run()
+
+from flask import Flask, render_template 
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('detalhesTelecomunicacoes.html')
+app.run()
+
