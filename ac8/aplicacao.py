03653b1ff07a0bc604394f5612d7b0ddc1e68d5f
felipeclau <felipeclau@github.com>
2019-10-23 12:04:39 -0300

    ajustes

--- ac8/aplicacao.py
+++ ac8/aplicacao.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index.html')
 def index():
     return render_template("index.html")
 
@@ -55,13 +55,13 @@ def matrizTelecomunicacoes():
 
 
 
-@app.route('/listaCurso')
+@app.route('/listaCurso.html')
 def listaCurso():
     return render_template('listaCurso.html')
 
 
 
-@app.route('/noticias')
+@app.route('/noticias.html')
 def noticias():
     return render_template('noticias.html')
 app.run()
