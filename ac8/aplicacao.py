0a8ae0fe4ee3b4ec0863bdefb935f7ac352cd335
felipeclau <felipeclau@github.com>
2019-10-23 11:06:17 -0300

    ajustes

--- ac8/aplicacao.py
+++ ac8/aplicacao.py
@@ -2,75 +2,75 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/detalhesEletronica')
+def eletronica():
     return render_template('detalhesEletronica.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/detalhesInformaticaIndustrial')
+def informaticaIndustrial():
     return render_template('detalhesInformáticaIndustrial.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/detalhesMecatronica')
+def mecatronica():
     return render_template('detalhesMecatronica.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/detalhesTelecomunicacoes')
+def telecomunicacoes():
     return render_template('detalhesTelecomunicacoes.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/matrizCurricular_Eletronica')
+def matrizEletronica():
     return render_template('matrizCurricular_Eletronica.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/matrizCurricular_InformaticaIndustrial')
+def matrizInformaticaIndustrial():
     return render_template('matrizCurricular_InformaticaIndustrial.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/matrizCurricular_Mecatronica')
+def matrizMecatronica():
     return render_template('matrizCurricular_Mecatronica.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
-@app.route('/')
-def index():
+@app.route('/matrizCurricular_Telecomunicacoes')
+def matrizTelecomunicacoes():
     return render_template('matrizCurricular_Telecominucacoes.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
 @app.route('/listaCurso')
-def index():
+def listaCurso():
     return render_template('listaCurso.html')
 app.run()
 
 from flask import Flask, render_template 
 app = Flask(__name__)
 @app.route('/noticias')
-def index():
+def noticias():
     return render_template('noticias.html')
 app.run()
